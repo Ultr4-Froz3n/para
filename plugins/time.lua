fcc97aa9ca21baf8bc35b5a9470d2d3ddba8d8ff
PARADOXTEAM <sajad.yas1377@gmail.com>
2016-07-26 01:16:22 +0430

    Update time.lua

--- plugins/time.lua
+++ plugins/time.lua
@@ -1,21 +1,11 @@
---[[
-#
-#    Time And Date
-#
-#    @WaderTGTeam
-# @WaderTGTeam
-#
-#
-]]
-
 function run(msg, matches)
 local url , res = http.request('http://api.gpmod.ir/time/')
 if res ~= 200 then return "No connection" end
 local jdat = json:decode(url)
-local text = '>>Time: '..jdat.FAtime..' \n>>Today: '..jdat.FAdate..'\n------------------\n>> '..jdat.ENtime..'\n>> '..jdat.ENdate.. '\n------------------\n@SoftTG'
+local text = '🕒 ساعت : '..jdat.FAtime..' \n📆 امروز '..jdat.FAdate..' میباشد.\n----------------------------\n🕒 '..jdat.ENtime..'\n📆 '..jdat.ENdate..'\n@Paradox_TEAM\nραяαđøχ вøт'
 return text
 end
 return {
-  patterns = {"^[#/!]([Tt][iI][Mm][Ee])$"}, 
+  patterns = {"^[/!]([Tt][iI][Mm][Ee])$"}, 
 run = run 
 }
