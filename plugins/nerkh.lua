5a7b6148a95b818a0269780f1b5007f4b351485a
PARADOXTEAM <sajad.yas1377@gmail.com>
2016-07-26 00:29:07 +0430

    Update nerkh.lua

--- plugins/nerkh.lua
+++ plugins/nerkh.lua
@@ -1,7 +1,7 @@
 do
 
 function run(msg, matches)
-  return "ادد کردن بات در گروها فعلا رایگان فقط به شرط های زیر؛\n--------------------------\n1.Gp Members (1_1k):Add 20 Member In Group Support\n2.Gp Members (1k_1.5k):Add 25 Member In Group Support\n3.Gp Members (1.5k_2k):Add 30 Member In Group Support\n4.Gp Members (2k_2.5k):Add 35 Member In Group Support\n5.Gp Members (2.5_3k):Add 40 Member In Group Support\n6.Gp Member Up (3k):Add 50 Member In Group Support\n--------------------------\nبرای خرید و سفارش گروه وارد کانال زیر شده و به پی وی ادمین مراجعه کنین:\n@Paradox_TEAM\n--------------------------\nParadoxBot"
+  return "ادد کردن بات در گروها فعلا رایگان فقط به شرط های زیر؛\n--------------------------\n1-Gp Members (1_1k):Add 20 Member In Group Support\n2-Gp Members (1k_1.5k):Add 25 Member In Group Support\n3-Gp Members (1.5k_2k):Add 30 Member In Group Support\n4-Gp Members (2k_2.5k):Add 35 Member In Group Support\n5-Gp Members (2.5_3k):Add 40 Member In Group Support\n6-Gp Member Up (3k):Add 50 Member In Group Support\n--------------------------\nبرای خرید و سفارش گروه وارد کانال زیر شده و به پی وی ادمین مراجعه کنین:\n@Paradox_TEAM\n--------------------------\nParadoxBot"
 end
 
 return {
